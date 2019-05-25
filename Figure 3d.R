b2b911abe9c843509ecb051e1c2423aa76a776bc
paolocrosetto <paolo.crosetto@gmail.com>
2019-05-25 21:31:36 +0200

    Figure 3d

--- Figure 3d.R
+++ Figure 3d.R
@@ -6,27 +6,34 @@
 
 ### This file produces Figure 3d of the paper -- the plot on the top third row of Figure 3
 
-## TODO: fix colors, update and comment code
+## TODO: change image in the file to be identical to this one!
 
-ns <- df %>% select(subject, caddy, treatment, product, quantity, "saturated fatty acids" = couleur_ags_NC, sugar = couleur_sucres_NC, salt = couleur_sel_NC) %>% 
+## selecting the relevant variables and rows
+ns <- df %>% select(subject, caddy, treatment, product, quantity, 
+                    "saturated fatty acids" = couleur_ags_NC, 
+                    sugar = couleur_sucres_NC, 
+                    salt = couleur_sel_NC) %>% 
   filter(quantity !=0) %>% 
-  filter(treatment == "NutriScore") %>% as_tibble() 
+  filter(treatment == "NutriScore") %>% 
+  as_tibble() 
 
-
-ns <- ns %>% ungroup() %>% select(-treatment) %>% mutate(caddy = as.factor(as.character(caddy))) %>% group_by(caddy) %>% 
-  gather(key, value, -subject, -product, -caddy, -quantity) %>% group_by(subject, caddy, key, value) %>% 
+## counting the number of products of each color for each nutrient of the Traffic Light
+ns <- ns %>% 
+  ungroup() %>% 
+  select(-treatment) %>% 
+  mutate(caddy = as.factor(as.character(caddy))) %>% 
+  group_by(caddy) %>% 
+  gather(key, value, -subject, -product, -caddy, -quantity) %>% 
+  group_by(subject, caddy, key, value) %>% 
   summarise(n = sum(quantity))
 
+## dealing with unlabeled products
+ns <- ns %>% mutate(value = if_else(is.na(value), "no label", value))
 
-ns$value[ns$value == ""] <- "No label"
-
-
-ns <- ns %>% spread(caddy,n)
-
-ns$`2`[is.na(ns$`2`)]<-0
-ns$`1`[is.na(ns$`1`)]<-0
-
-ns <- ns %>%  mutate(diff = `2` - `1`) %>% 
+## compute difference varibale for each nutrient and summairze to mean and confidence interval
+ns <- ns %>% 
+  spread(caddy,n, fill = 0)
+  mutate(diff = `2` - `1`) %>% 
   group_by(key, value) %>% 
   summarise(mdiff = mean(diff, na.rm = T), sediff = sd(diff, na.rm = T)/sqrt(n())) %>% 
   mutate(cil = mdiff-2*sediff, cih = mdiff + 2*sediff) %>% 
@@ -40,5 +47,5 @@ ns %>% ggplot(aes(value, mdiff, fill=value))+geom_col()+theme_minimal()+facet_gr
        theme(strip.text = element_text(face = "bold"), legend.position = "none")+
        xlab("NutriCouleur color, by nutrient")+ylab("Mean number of changes, caddy 2 vs caddy 1")+
        ggtitle("Mean number of product changes by NutriCouleur label color, by subjects and nutrient")
-ggsave("Behavioral_NC_individual.png", width = 8, height = 5, units = "in", dpi = 300)
+ggsave("Figure_3d.png", width = 8, height = 5, units = "in", dpi = 300)
 
